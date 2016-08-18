ec68585fab1203e7f960db0572988ebaaf47fb82
aleeex <akang2@gmail.com>
2016-08-18 14:02:28 -0600

    added new attr to html

--- public/app.js
+++ public/app.js
@@ -8,8 +8,15 @@ angular.module('s3Demo', [])
 
         //the line below is what converts the file to base64. IMPORTANT
         //this happens asynchronously. important cause some images
-        //are fuckin huge. 
+        //are fuckin huge.
         reader.readAsDataURL(changeEvent.target.files[0]);
+
+        //code below is goign to take in event, convert it and
+        reader.onload=function(loadEvent) {
+          //loadEvent is your image in binary/object
+          var rawData = loadEvent.target.result;
+          console.log(rawData);
+        }
       })
     }
   }
