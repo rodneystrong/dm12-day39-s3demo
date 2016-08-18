ad09f2640d872c1d0d8a97db9548bb7607dc0ab5
aleeex <akang2@gmail.com>
2016-08-18 13:56:41 -0600

    added our event listener

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 angular.module('s3Demo', [])
 .directive('fileRead', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs) {
+      elem.on('change', function(changeEvent) {
+        var reader = new FileReader();
+
+        reader.readAsDataURL()
+      })
+    }
+  }
 
 })
 .service('dataService', function($http) {
