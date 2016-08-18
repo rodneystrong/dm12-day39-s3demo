32ffa4843f1a446367ff5d18458ab753ff54b2a4
aleeex <akang2@gmail.com>
2016-08-18 14:11:35 -0600

    had to comment out my shit to get someone elses code to work. need to review/textcompare later

--- public/app.js
+++ public/app.js
@@ -1,26 +1,42 @@
 angular.module('s3Demo', [])
+// .directive('fileRead', function() {
+//   return {
+//     restrict: 'A',
+//     link: function(scope, elem, attrs) {
+//       elem.on('change', function(changeEvent) {
+//         var reader = new FileReader();
+//
+//         //the line below is what converts the file to base64. IMPORTANT
+//         //this happens asynchronously. important cause some images
+//         //are fuckin huge.
+//         reader.readAsDataURL(changeEvent.target.files[0]);
+//
+//         //code below is goign to take in event, convert it and
+//         reader.onload=function(loadEvent) {
+//           //loadEvent is your image in binary/object
+//           var rawData = loadEvent.target.result;
+//           console.log(rawData);
+//         }
+//       })
+//     }
+//   }
+//
+// })
+//
 .directive('fileRead', function() {
-  return {
-    restrict: 'A',
-    link: function(scope, elem, attrs) {
-      elem.on('change', function(changeEvent) {
-        var reader = new FileReader();
-
-        //the line below is what converts the file to base64. IMPORTANT
-        //this happens asynchronously. important cause some images
-        //are fuckin huge.
-        reader.readAsDataURL(changeEvent.target.files[0]);
-
-        //code below is goign to take in event, convert it and
-        reader.onload=function(loadEvent) {
-          //loadEvent is your image in binary/object
-          var rawData = loadEvent.target.result;
-          console.log(rawData);
-        }
-      })
-    }
-  }
-
+  return {
+       restrict: 'A',
+       link: function(scope, elem, attrs) {
+           elem.on('change', function(changeEvent){
+               var reader = new FileReader();
+               reader.readAsDataURL(changeEvent.target.files[0])
+               reader.onload = function(loadEvent) {
+                   var rawData = loadEvent.target.result;
+                   console.log(rawData);
+               }
+           })
+       }
+   }
 })
 .service('dataService', function($http) {
 
